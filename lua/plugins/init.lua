81a1768e2836b449dd5da6d3bf9a211ee46e35ef
justbarnt <justbarnt@icloud.com>
2023-10-12 16:47:14 -0400

    Attempting to fix LSP again

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,7 +14,7 @@ return {
             })
         end
     },
-
+        
     -- Accurate Code Syntax Highlighing
     {
         "nvim-treesitter/nvim-treesitter",
@@ -32,96 +32,36 @@ return {
         "VonHeikemen/lsp-zero.nvim",
         branch = "v3.x",
         lazy = true,
-        config = false,
-        init = function()
-            vim.g.lsp_zero_extend_cmp = 0
-            vim.g.lsp_zero_extend_lspconfig = 0
-        end,
-    },
-    {
-        "williamboman/mason.nvim",
-        lazy = false,
-        config = true
-    }, 
-
-    -- Autocompletion
-    {
-        'hrsh7th/nvim-cmp',
-        event = 'InsertEnter',
-        dependencies = {
-            {
-                'L3MON4D3/LuaSnip',
-                "rafamadriz/friendly-snippets",
-                "hrsh7th/nvim-cmp",
-                "hrsh7th/cmp-buffer",
-                "hrsh7th/cmp-path",
-                "saadparwaiz1/cmp_luasnip",
-                "hrsh7th/cmp-nvim-lsp",
-                "hrsh7th/cmp-nvim-lua",
-            },
-        },
         config = function()
-            -- TODO: Move into after/plugings/autocomplete
-            -- Here is where you configure the autocompletion settings.
             local lsp_zero = require('lsp-zero')
-            lsp_zero.extend_cmp()
-
-            -- And you can configure cmp even more, if you want to.
-            local cmp = require('cmp')
-            local cmp_action = lsp_zero.cmp_action()
-
-            cmp.setup({
-                formatting = lsp_zero.cmp_format(),
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-                    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-                    ['<C-e>'] = cmp.mapping.abort(),
-                })
-            })
-        end
-    },
-
-    -- LSP
-    {
-        'neovim/nvim-lspconfig',
-        cmd = {'LspInfo', 'LspInstall', 'LspStart'},
-        event = {'BufReadPre', 'BufNewFile'},
-        dependencies = {
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'williamboman/mason-lspconfig.nvim'},
-            {"hrsh7th/nvim-cmp"},
-            {"hrsh7th/cmp-buffer"},
-            {"hrsh7th/cmp-path"},
-        },
-        config = function()
-            -- TODO: Move to after/plugins/lsp
-            -- This is where all the LSP shenanigans will live
-            local lsp_zero = require('lsp-zero')
-            lsp_zero.extend_lspconfig()
 
             lsp_zero.on_attach(function(client, bufnr)
-                -- see :help lsp-zero-keybindings
-                -- to learn the available actions
                 lsp_zero.default_keymaps({buffer = bufnr})
             end)
 
-            require('mason-lspconfig').setup({
-                ensure_installed = {},
-                handlers = {
-                    lsp_zero.default_setup,
-                    lua_ls = function()
-                        -- (Optional) Configure lua language server for neovim
-                        local lua_opts = lsp_zero.nvim_lua_ls()
-                        require('lspconfig').lua_ls.setup(lua_opts)
-                    end,
-                }
+            require('lspconfig').powershell_es.setup({
+                bundle_path = "C:\\PowerShellEditor\\PowerShellEditorServices"
             })
-        end
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+            lsp_zero.setup_servers({'cmake', 'emmet_language_server', 'eslint', 'html', 'intelephense', 'jsonls', 'lua_ls', 'svelte', 'tailwindcss', 'tsserver', })
+        end,
     },
-        
+
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            {'hrsh7th/cmp-nvim-lsp'},
+        }
+    },
+
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            {'L3MON4D3/LuaSnip'}  
+        },
+    },
+
     -- Fuzzy Finder
     {
         "nvim-telescope/telescope.nvim",
