84ab6ccc6bdfaaf61751091790ca4b0d0a5b44b2
Brent Williams <bwilliams@ad.commsys.com>
2023-10-24 10:02:49 -0400

    added task

--- init.lua
+++ init.lua
@@ -11,7 +11,7 @@ keymaps.plugins()
 if vim.g.neovide then
     require 'core.neovide'
 end
-
+-- add in vim.g.vscode to prevent plugins from loading that don't work in vscode
 
 require("core.lazy") -- Setup Lazy plugin manager
 require("core.colorscheme") -- Set colorscheme
