6cc985d02f94314e45f2c40257ee24ad07faaef1
Brent Williams <bwilliams@ad.commsys.com>
2023-10-26 11:20:16 -0400

    updated init to not load plugins if you're in vscode

--- init.lua
+++ init.lua
@@ -6,13 +6,15 @@ require("core.options")
 -- Get and active our keymaps
 local keymaps = require("core.keymaps")
 keymaps.init()
-keymaps.plugins()
 
 if vim.g.neovide then
-    require 'core.neovide'
-end
--- add in vim.g.vscode to prevent plugins from loading that don't work in vscode
+    if not vim.g.vscode then
+        keymaps.plugins()
+        require 'core.neovide'
+        -- add in vim.g.vscode to prevent plugins from loading that don't work in vscode
 
-require("core.lazy") -- Setup Lazy plugin manager
-require("core.colorscheme") -- Set colorscheme
-require("core.autocmds")
+        require("core.lazy") -- Setup Lazy plugin manager
+        require("core.colorscheme") -- Set colorscheme
+        require("core.autocmds")
+    end
+end
