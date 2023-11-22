888f5f372e3a4ce0ec45d6cb849fdb76b3d9c82a
justbarnt <justbarnt@icloud.com>
2023-11-22 10:30:45 -0500

    Added git setup

---
+++ after/plugin/git.lua
@@ -0,0 +1,33 @@
+---[[ - ]] vim.fn["gina#custom#command#option"]('status', '--opener', 'vsplit')
+-- nnoremap { '<leader>gs', '<cmd>Gina status<CR>' }
+
+local ok, neogit = pcall(require, "neogit")
+if not ok then
+    return
+end
+
+neogit.setup({
+    integrations = {
+        diffview = true,
+    },
+})
+
+vim.keymap.set("n", "<space>vv", ":DiffviewOpen<CR>", { desc = "Diff View Open" })
+
+vim.keymap.set("n", "<leader>gs", neogit.open, { desc = "[G]it [S]tatus" })
+vim.keymap.set("n", "<leader>gc", function()
+    neogit.open({ "commit" })
+end, {
+    desc = "[G]it [C]ommit",
+})
+
+-- hi NeogitNotificationInfo guifg=#80ff95
+-- hi NeogitNotificationWarning guifg=#fff454
+-- hi NeogitNotificationError guifg=#c44323
+-- hi def NeogitDiffAddHighlight guibg=#404040 guifg=#859900
+-- hi def NeogitDiffDeleteHighlight guibg=#404040 guifg=#dc322f
+-- hi def NeogitDiffContextHighlight guibg=#333333 guifg=#b2b2b2
+-- hi def NeogitHunkHeader guifg=#cccccc guibg=#404040
+-- hi def NeogitHunkHeaderHighlight guifg=#cccccc guibg=#4d4d4d
+
+-- git config --global merge.conflictStyle diff3
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,8 +1,16 @@
 return {
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+            "sindrets/diffview.nvim",
+            "nvim-telescope/telescope-fzf-native.nvim",
+        },
+        config = true,
+    },
     "lewis6991/gitsigns.nvim",
-    "rhysd/git-messenger.vim",
-    "rhysd/committia.vim",    -- Commit msgs
+    "rhysd/committia.vim", -- Commit msgs
     "sindrets/diffview.nvim", -- Diff helper
-    "Rawnly/gist.nvim"        -- GIST HELPER
+    "Rawnly/gist.nvim", -- GIST HELPER
 }
-
