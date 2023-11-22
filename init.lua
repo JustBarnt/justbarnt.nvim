a15baf2ca3babf31851b2f95d7d5b11273c42ecb
Brent Williams <bwilliams@ad.commsys.com>
2023-11-22 09:56:01 -0500

    Removed .luarc.json from gitignore idk why it was there in the first place...

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 -- Set our preferred options
 require("core.options")
-
 -- Get and active our keymaps
 local keymaps = require("core.keymaps")
 keymaps.init() -- Base keybinds
@@ -8,7 +7,7 @@ keymaps.init() -- Base keybinds
 if not vim.g.vscode then
     --keymaps.plugins() -- run plugin related keybinds
     -- add in vim.g.vscode to prevent plugins from loading that don't work in vscode
-    require("core.lazy")        -- Setup Lazy plugin manager
+    require("core.lazy") -- Setup Lazy plugin manager
     require("core.colorscheme") -- Set colorscheme
     -- require("core.autocmds") -- Set auto commands
 end
