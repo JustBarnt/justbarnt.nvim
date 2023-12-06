47926d834eb2bdca8906c66e1c94dbb326f511ee
justbarnt <justbarnt@icloud.com>
2023-12-06 16:53:00 -0500

    Updated theprimeagen/harpoon -> theprimeagen/harpoon2

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,14 +1,26 @@
-local status, harpoon = pcall(require, "harpoon")
+local harpoon = require("harpoon")
 
-if not status then
-    return
+-- REQUIRED
+harpoon:setup({})
+-- REQUIRED
+
+-- Append current open file to our harpon list
+vim.keymap.set("n", "<leader>ha", function()
+    harpoon:list():append()
+end, { desc = "[H]arpoon [A]dd", silent = true })
+
+-- Toggle a UI popup of or current harpooned items.
+vim.keymap.set("n", "<leader>hl", function()
+    harpoon.ui:toggle_quick_menu(harpoon:list())
+end, { desc = "[H]arpoon [L]ist", silent = true })
+
+for i = 1, 5 do
+    vim.keymap.set("n", string.format("<space>%s", i), function()
+        harpoon:list():select(i)
+    end, { desc = "Harpoon: Jump To File: " .. i, silent = true })
 end
 
--- REQUIRED
-harpoon.setup()
--- REQUIRED
-
-vim.keymap.set("n", "<leader>ha", function()
+--[[ vim.keymap.set("n", "<leader>ha", function()
     require("harpoon.mark").add_file()
 end, { desc = "[H]arpoon [A]dd" })
 
@@ -22,4 +34,4 @@ for i = 1, 5 do
     end, {
         desc = "Harpoon: Jump To File: " .. i,
     })
-end
+end ]]
