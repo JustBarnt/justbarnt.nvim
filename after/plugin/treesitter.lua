17edd3a7e4d818d65ef4d568d0124a9f48989727
justbarnt <justbarnt@icloud.com>
2023-12-14 10:20:25 -0500

    Fixed Stupid Issue where treesitter setup file was not in the right folder... therefore did not work...

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -3,10 +3,31 @@ if not pcall(require, "nvim-treesitter") then
     return
 end
 
+-- Just contains the list of all current parsers
 local list = require("nvim-treesitter.parsers").get_parser_configs()
 
----@diagnostic_disabled: missing-fields
-local _ = require("nvim-treesitter.configs").setup({
+local swap_next, swap_prev = (function()
+    local swap_objects = {
+        p = "@parameter.inner",
+        f = "@function.outer",
+        e = "@element",
+
+        -- Not ready yet but @tjdevries (telescope creators) fault
+        -- v = "@variable"
+    }
+
+    local n, p = {}, {}
+
+    for key, obj in pairs(swap_objects) do
+        n[string.format("<M-Space><M-%s>", key)] = obj
+        p[string.format("<M-BS><M-%s>", key)] = obj
+    end
+
+    return n, p
+end)()
+
+---@diagnostic disable: missing-fields
+require("nvim-treesitter.configs").setup({
     ensure_installed = {
         "html",
         "javascript",
@@ -23,15 +44,20 @@ local _ = require("nvim-treesitter.configs").setup({
         "c",
         "cpp",
         "vimdoc",
+        "php",
     },
-
-    highlight = { enable = true },
+    indent = {
+        enable = true,
+    },
+    highlight = {
+        enable = true,
+    },
     refactor = {
         highlight_definitions = { enable = true },
         highlight_current_scope = { enable = false },
 
         smart_rename = {
-            enable = true,
+            enable = false,
             keymaps = {
                 smart_rename = "grr",
             },
@@ -46,14 +72,15 @@ local _ = require("nvim-treesitter.configs").setup({
         },
     },
 
-    context_commentstring = {
+    incremental_selection = {
         enable = true,
-
-        -- With Comment.nvim, we don't need to run this on the autocmd
-        --  Only run it in pre-hook
-        enable_autocmd = false,
+        keymaps = {
+            init_selection = "<M-w>",
+            node_incremental = "<M-w>",
+            node_decremental = "<M-C-w>",
+            scope_incremental = "<M-e>",
+        },
     },
-
     textobjects = {
         move = {
             enable = true,
@@ -105,6 +132,14 @@ local _ = require("nvim-treesitter.configs").setup({
         },
     },
 })
+
+require("ts_context_commentstring").setup({
+    -- With Comment.nvim, we don't need to run this on the autocmd
+    --  Only run it in pre-hook
+    enable_autocmd = false,
+})
+
+require("treesitter-context").setup({ enable = true })
 
 vim.treesitter.query.set("javascript", "context", "")
 vim.treesitter.query.set("typescript", "context", "")
