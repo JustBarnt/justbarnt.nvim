30e447fc547bce30b77b7960fa1684d3aa377503
justbarnt <justbarnt@icloud.com>
2023-12-26 10:41:32 -0500

    Added base file for nvim-surroun, if I decide to customize it. Add luarc.json to ignore list.

---
+++ after/plugin/nvim-surround.lua
@@ -0,0 +1,8 @@
+local surround = require("nvim-surround")
+local input = require("nvim-surround.input")
+local functional = require("nvim-surround.functional")
+
+---@class user_options
+local opts = {}
+
+surround.setup(opts)
